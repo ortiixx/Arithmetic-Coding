a88a65736396aaa3dcdffba186951e762102cbba
Albert Ortiz Guillem <albert.ortiz.guillem@a5s109pc12.fib.upc.es>
2019-04-10 13:49:14 +0200

    Falta corregir bug

--- 03.1_Aritmetica_Entera_PRACTICA.py
+++ 03.1_Aritmetica_Entera_PRACTICA.py
@@ -44,8 +44,6 @@ def IntegerArithmeticCode(mensaje,alfabeto,frecuencias):
         u = uax
         c1 = (u & b) == (l & b)
         c2 = ((u & b2) != (l & b2)) and ((l & b2) == b2)
-        print("U: ", u)
-        print("L: ", l)
         while(c1 | c2):
             if(c1):
                 tbit = ''
@@ -68,7 +66,7 @@ def IntegerArithmeticCode(mensaje,alfabeto,frecuencias):
             #here we update the conditions
             c1 = (u & b) == (l & b)
             c2 = ((u & b2) != (l & b2)) and ((l & b2) == b2)
-
+        transmission += '0'+'1'*scale+'0'*(m-1)
     return transmission
     
     
@@ -135,14 +133,26 @@ Definir otra función que decodifique los mensajes codificados con la función
 anterior.
 '''
 
+def TransformarMovida(mensaje):
+    dick = {}
+    for c in mensaje:
+        if c in dick: dick[c] += 1
+        else: dick[c] = 1
+    dicks = sorted(dick, key = lambda k: int(dick[k]))
+    alfa = dicks
+    freqs = [dick[c] for c in alfa]
+    return alfa, freqs
 
 def EncodeArithmetic(mensaje_a_codificar):
-
-    return mensaje_codificado,alfabeto,frecuencias
+    alfa, freqs = TransformarMovida(mensaje_a_codificar)
+    print(alfa)
+    print(freqs)
+    mensaje_codificado = IntegerArithmeticCode(mensaje_a_codificar,alfa,freqs)
+    return mensaje_codificado,alfa,freqs
     
 def DecodeArithmetic(mensaje_codificado,tamanyo_mensaje,alfabeto,frecuencias):
 
-    return mensaje_decodificado
+    return IntegerArithmeticDecode(mensaje_codificado,tamanyo_mensaje,alfabeto,frecuencias)
         
 #%%
 '''
@@ -157,14 +167,10 @@ frecuencias=[1,10,20,300]
 mensaje='dddcabccacabadac'
 tamanyo_mensaje=len(mensaje)  
 code = IntegerArithmeticCode(mensaje, alfabeto, frecuencias)
-print(code)
-print(lista_C[0])
-print(IntegerArithmeticDecode(code, tamanyo_mensaje, alfabeto, frecuencias))
 
-
-#for C in lista_C:
-#    mensaje_recuperado=DecodeArithmetic(C,tamanyo_mensaje,alfabeto,frecuencias)
-#    print(mensaje==mensaje_recuperado)
+for C in lista_C:
+    mensaje_recuperado=DecodeArithmetic(C,tamanyo_mensaje,alfabeto,frecuencias)
+    print(mensaje==mensaje_recuperado)
 
 
 
@@ -176,13 +182,14 @@ Ejemplo
 '''
 
 mensaje='La heroica ciudad dormía la siesta. El viento Sur, caliente y perezoso, empujaba las nubes blanquecinas que se rasgaban al correr hacia el Norte. En las calles no había más ruido que el rumor estridente de los remolinos de polvo, trapos, pajas y papeles que iban de arroyo en arroyo, de acera en acera, de esquina en esquina revolando y persiguiéndose, como mariposas que se buscan y huyen y que el aire envuelve en sus pliegues invisibles. Cual turbas de pilluelos, aquellas migajas de la basura, aquellas sobras de todo se juntaban en un montón, parábanse como dormidas un momento y brincaban de nuevo sobresaltadas, dispersándose, trepando unas por las paredes hasta los cristales temblorosos de los faroles, otras hasta los carteles de papel mal pegado a las esquinas, y había pluma que llegaba a un tercer piso, y arenilla que se incrustaba para días, o para años, en la vidriera de un escaparate, agarrada a un plomo. Vetusta, la muy noble y leal ciudad, corte en lejano siglo, hacía la digestión del cocido y de la olla podrida, y descansaba oyendo entre sueños el monótono y familiar zumbido de la campana de coro, que retumbaba allá en lo alto de la esbeltatorre en la Santa Basílica. La torre de la catedral, poema romántico de piedra,delicado himno, de dulces líneas de belleza muda y perenne, era obra del siglo diez y seis, aunque antes comenzada, de estilo gótico, pero, cabe decir, moderado por uninstinto de prudencia y armonía que modificaba las vulgares exageraciones de estaarquitectura. La vista no se fatigaba contemplando horas y horas aquel índice depiedra que señalaba al cielo; no era una de esas torres cuya aguja se quiebra desutil, más flacas que esbeltas, amaneradas, como señoritas cursis que aprietandemasiado el corsé; era maciza sin perder nada de su espiritual grandeza, y hasta sussegundos corredores, elegante balaustrada, subía como fuerte castillo, lanzándosedesde allí en pirámide de ángulo gracioso, inimitable en sus medidas y proporciones.Como haz de músculos y nervios la piedra enroscándose en la piedra trepaba a la altura, haciendo equilibrios de acróbata en el aire; y como prodigio de juegosmalabares, en una punta de caliza se mantenía, cual imantada, una bola grande debronce dorado, y encima otra más pequenya, y sobre ésta una cruz de hierro que acababaen pararrayos.'
-#mensaje_codificado,alfabeto,frecuencias=EncodeArithmetic(mensaje)
-#mensaje_recuperado=DecodeArithmetic(mensaje_codificado,len(mensaje),alfabeto,frecuencias)
 
-#ratio_compresion=8*len(mensaje)/len(mensaje_codificado)
-#print(ratio_compresion)
+mensaje_codificado,alfabeto,frecuencias=EncodeArithmetic(mensaje)
+mensaje_recuperado=DecodeArithmetic(mensaje_codificado,len(mensaje),alfabeto,frecuencias)
 
-#if (mensaje!=mensaje_recuperado):
-#        print('!!!!!!!!!!!!!!  ERROR !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!')
+ratio_compresion=8*len(mensaje)/len(mensaje_codificado)
+print(ratio_compresion)
+
+if (mensaje==mensaje_recuperado):
+        print('!!!!!!!!!!!!!!  IGUALES !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!')
         
         
